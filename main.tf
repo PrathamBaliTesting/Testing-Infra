b5f822217a057299c52deb9b582953bfa6c664e0
pratham-baliyan-3057 <pratham.baliyan@octobit8.com>
2024-08-16 00:11:34 +0530

    modified:   main.tf 	modified:   outputs.tf 	modified:   variables.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,57 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   
   tags={
-    name=my_vpc
+    name=Bali_vpc
   }
+}
+
+resource "aws_subnet" "public_subnets" {
+  count=length(var.subnet_public_cidrs)
+  vpc_id = aws_vpc.main.id
+  cidr_block = element(var.subnet_public_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+
+  tags={
+    Name="Public Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count=length(var.subnet_private_cidrs)
+  vpc_id = aws_vpc.main.id
+  cidr_block = element(var.subnet_private_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+  tags={
+    Name="Private Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags={
+    Name="Bali_vpc_IG"
+  }
+  
+}
+
+resource "aws_route_table" "second_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "2nd Route Table"
+  }
+  
+}
+
+resource "aws_route_table_association" "public_subnets_asso" {
+  count = length(var.subnet_public_cidrs)
+  subnet_id = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.second_rt.id
+  
 }
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "subnet_public_cidrs" {
+  type=list(string)
+  description = "Public Subnet CIDR values"
+  default = [ "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24","10.0.4.0/24","10.0.5.0/24","10.0.6.0/24" ]
+  
+}
+
+variable "subnet_private_cidrs" {
+  type=list(string)
+  description = "Private Subnet CIDR values"
+  default = [ "10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24","10.0.4.0/24","10.0.5.0/24","10.0.6.0/24" ]
+
+  
+}
+#AZS --> Availability Zones
+variable "azs" {
+  type=list(string)
+  description = "Availability Zones"
+  default = [ "us-east-1a","us-east-1b","us-east-1c","us-east-1d","us-east-1e","us-east-1f" ]
+  
+}
