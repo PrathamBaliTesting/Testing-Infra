6a344a9a58be40d783726f90020c609ecd6f9ec5
pratham-baliyan-3057 <pratham.baliyan@octobit8.com>
2024-08-16 00:54:06 +0530

    modified:   outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,20 @@
-output "vpcs" {
-    description = "VPC Outputs"
-    value ={for vpc in aws_vpc.main : vpc.tags.Name => {"cidr_block" : vpc.cidr_block, "id": vpc.id}}
-  
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
 }
+
+output "public_subnet_id" {
+  description = "The ID of the public subnet"
+  value       = aws_subnet.public.subnet.id
+}
+
+output "private_subnet_id" {
+  description = "The ID of the private subnet"
+  value       = aws_subnet.private.subnet.id
+}
+
+output "internet_gateway_id" {
+  description = "The ID of the Internet Gateway"
+  value       = aws_internet_gateway.main.id
+}
+
