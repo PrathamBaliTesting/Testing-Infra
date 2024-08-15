f3578c2ec52cf29ccf08111e95b6f87ccdb2dab7
pratham-baliyan-3057 <pratham.baliyan@octobit8.com>
2024-08-16 00:24:54 +0530

    modified:   outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "vpcs" {
     description = "VPC Outputs"
-    value ={for vpc in aws_vpc.main : vpc.tags.Name => {"cidr_block" : vpc.cidr_block, "id": vpc_id}}
+    value ={for vpc in aws_vpc.main : vpc.tags.Name => {"cidr_block" : vpc.cidr_block, "id": vpc.id}}
   
 }
