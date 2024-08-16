6d7ad619df019dd4929b41b1dd0e373086e74e59
pratham-baliyan-3057 <pratham.baliyan@octobit8.com>
2024-08-16 11:03:19 +0530

    modified:   variables.tf

--- variables.tf
+++ variables.tf
@@ -14,14 +14,14 @@ variable "vpc_cidr" {
 variable "subnet_public_cidrs" {
   type=string
   description = "Public Subnet CIDR values"
-  default = "10.0.1.0/16"
+  default = "10.0.1.0/24"
   
 }
 
 variable "subnet_private_cidrs" {
   type=string
   description = "Private Subnet CIDR values"
-  default =  "10.0.2.0/16" 
+  default =  "10.0.2.0/24" 
 
   
 }
