dc5ea1cbb20010a36291439e96a8f4d75b9ebc5b
pratham-baliyan-3057 <pratham.baliyan@octobit8.com>
2024-08-16 10:59:31 +0530

    modified:   main.tf 	modified:   variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "region" {
+  description = "The AWS region to deploy the VPC"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "vpc_cidr" {
   description = "The CIDR block for the VPC"
   type        = string
